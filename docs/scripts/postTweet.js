14f7f14979a1c5268113c188f37e7dbb569c79b1
Andy-Lopez <adnylopez@gmail.com>
2021-07-31 11:43:52 -0700

    add style to post tweet

--- docs/scripts/postTweet.js
+++ docs/scripts/postTweet.js
@@ -5,11 +5,11 @@ const tweetButton = document.getElementById('postTweetButton')
 
 tweetButton.addEventListener('click', (e) => {
   e.preventDefault()
-alert("Tweet posted: " + postForm.fname.value)
+alert("Tweet posted: " + postForm.tweet.value)
   const userId = 4
-  let text = postForm.fname.value
+  let text = postForm.tweet.value
   text = mockroblog.postMessage(userId, text)
-  if (text != null) {
+  if (text != null && typeof text !== 'undefined')  {
     window.location.replace('/usertimeline.html?username=self')
     localStorage.setItem('user_name', username)
   } else {
